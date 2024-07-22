e6a16b39585919bbde0bde0e82fd9a99b8d85453
Narendra Mahara <narendramahara85131@gmail.com>
2024-07-22 14:00:30 +0545

    logic added to the cross icon of the search field (chalne lagi)

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,12 @@
+// *********For the stickyPhone*********
 let stickyPhone = document.querySelector(".sticky-phone");
-
 let stickyPhoneDetails = document.querySelector(".sticky-phone-details");
 
+// ***For the clear input field in the search bar***
+let clearInput = document.querySelector(".cross-icon");
+let searchField = document.querySelector(".search-items");
+
+// *********For the stickyPhone*********
 stickyPhone.addEventListener("mouseenter", (event) => {
   if (event.target === stickyPhone) {
     stickyPhoneDetails.classList.remove("hide");
@@ -20,4 +25,10 @@ stickyPhoneDetails.addEventListener("mouseenter", () => {
 
 stickyPhoneDetails.addEventListener("mouseleave", () => {
   stickyPhoneDetails.classList.add("hide");
+});
+
+// ***For the clear input field in the search bar***
+
+clearInput.addEventListener("click", () => {
+  searchField.value = "";
 });
